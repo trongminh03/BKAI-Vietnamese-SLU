a46f977479eb4eb544d2cbfac261ca7c52cb27af
trongminh03 <minh140903@gmail.com>
2023-09-16 11:02:18 +0700

    update data

---
+++ slu_data_1/data-process/out_convert.py
@@ -0,0 +1,57 @@
+import re
+import json
+
+def label_tokens(text):
+    labels = []
+    
+    matches = re.finditer(r'\[ ([^:]+) : ([^\]]+) \]', text)
+    last_end = 0
+    number = 0
+    for match in matches:
+        start = match.start(2)
+        end = match.end(2)
+        label_type = match.group(1)
+        label_type = label_type.replace(' ', '_')
+        num_underscores = label_type.count('_')
+        label_value = match.group(2)
+        # Tokelabel_typetside the brackets and on the right side of the brackets
+        if (number == 0):
+            labels.extend(['O'] * (len(text[last_end:start].split()) - 3 - num_underscores))
+            number += 1
+        else:
+            labels.extend(['O'] * (len(text[last_end:start].split()) - 4 - num_underscores))
+        labels.extend(['B-' + label_type] + ['I-' + label_type] * (len(label_value.split()) - 1))
+        last_end = end
+    # Tokenize remaining words
+    labels.extend(['O'] * (len(text[last_end:].split()) - 1))
+    
+    return labels
+
+if __name__ == '__main__':
+    train_file_path = 'slu_data_1/data-process/train_processed.jsonl'
+    # Read data from the JSONL file
+    data = []
+    with open(train_file_path, 'r', encoding='utf-8') as jsonl_file:
+        for line in jsonl_file:
+            entry = json.loads(line)
+            data.append(entry)
+
+    with open('slu_data_1/data-process/seq.out', 'w', encoding='utf-8') as output_file:
+        for entry in data:
+            sentence_annotation = entry['sentence_annotation']
+            modified_seq = sentence_annotation.replace(',', ' ')
+            modified_seq = modified_seq.replace(']', ' ] ')
+            modified_seq = modified_seq.replace('[', ' [')
+            modified_seq = modified_seq.replace('\'',' ')
+            modified_seq = modified_seq.replace('.',' ')
+            modified_seq = modified_seq.replace('?', ' ')
+            modified_seq = modified_seq.replace('!',' ')
+            modified_seq = modified_seq.replace('/',' ')
+            labels = label_tokens(modified_seq)
+            
+            for label in labels:
+                output_file.write(f"{label} ")
+            output_file.write('\n')   
+
+            
+    print("Label sequences generated and saved to 'seq.out'")
---
+++ slu_data_1/data-process/split.py
@@ -0,0 +1,45 @@
+import json
+import random
+
+output_sentence_path = 'slu_data_1/data-process/seq.in'
+output_annotation_path = 'slu_data_1/data-process/seq.out'
+output_label_path = 'slu_data_1/data-process/label'
+
+def read_file(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def write_file(path, lines):
+    with open(path, 'w') as file:
+        for line in lines:
+            file.write(line)
+
+def split_train_test():
+    sentences = read_file(output_sentence_path)
+    annotations = read_file(output_annotation_path)
+    labels = read_file(output_label_path
+                       )
+    data_pairs = list(zip(sentences, annotations, labels))
+    random.shuffle(data_pairs)
+
+    train_data = data_pairs[:-2] 
+    dev_data = data_pairs[-2:-1]
+    test_data = data_pairs[-1:]
+
+    train_sentenes, train_annotations, train_labels = zip(*train_data)
+    test_sentenes, test_annotations, test_labels = zip(*test_data)
+    dev_sentenes, dev_annotations, dev_labels = zip(*dev_data)
+
+    write_file('slu_data_1/syllable-level/train/seq.in', train_sentenes)
+    write_file('slu_data_1/syllable-level/train/seq.out', train_annotations)
+    write_file('slu_data_1/syllable-level/train/label', train_labels)
+    write_file('slu_data_1/syllable-level/test/seq.in', test_sentenes)
+    write_file('slu_data_1/syllable-level/test/seq.out', test_annotations)
+    write_file('slu_data_1/syllable-level/test/label', test_labels)
+    write_file('slu_data_1/syllable-level/dev/seq.in', dev_sentenes)
+    write_file('slu_data_1/syllable-level/dev/seq.out', dev_annotations)
+    write_file('slu_data_1/syllable-level/dev/label', dev_labels)
+
+split_train_test()
+print("split successfully")
