015e035e5310e66a1327d78606f4590e1a0f9f6a
trongminh03 <minh140903@gmail.com>
2023-09-21 13:52:00 +0700

    update processed new data

--- extract_sentence.py
+++ extract_sentence.py
@@ -20,10 +20,8 @@ if __name__ == '__main__':
         for line in input_file:
             # Split the line based on space and take the last part as the extracted text
             parts = line.strip().split(" ")
-            if len(parts) > 1:
-                text = " ".join(parts[1:])
-
-                extracted_text.append(text)
+            text = " ".join(parts[1:])
+            extracted_text.append(text)
 
     # Open the output file for writing
     with open(output_file_path, "w", encoding="utf-8") as output_file:
